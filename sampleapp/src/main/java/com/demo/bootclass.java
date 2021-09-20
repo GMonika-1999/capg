1d5677fd81248d6b9b74731237ecb5afe8c46bf2
GMonika-1999 <gdnb1999@gmail.com>
2021-09-20 11:36:18 +0530

    changes

--- sampleapp/src/main/java/com/demo/bootclass.java
+++ sampleapp/src/main/java/com/demo/bootclass.java
@@ -4,7 +4,12 @@ public class bootclass {
 
 	public static void main(String[] args) {
 		System.out.println("hello");
+		show();
 
+	}
+	public static void show()
+	{
+		System.out.println("show method");
 	}
 
 }
